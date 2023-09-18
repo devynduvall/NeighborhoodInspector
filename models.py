433d7ba1da0320e97a5b7c618ce2859611a720f3
Devyn Duvall <35125134+devynduvall@users.noreply.github.com>
2023-09-17 20:53:35 -0700

    getting the environment back up and running

--- models.py
+++ models.py
@@ -1,32 +1,28 @@
-from app import app, db
-from sqlalchemy.sql import func
+from app import db
 
-# Rentals that are within Seattle, WA
 class Rental(db.Model):
-    rental_id = db.Column(db.String(50), primary_key = True)
-    address = db.Column(db.String(120), index = True, unique = False)
-    price = db.Column(db.String(50), index = False, unique = False)
-    #list_date = db.Columns(db.DateTime, index = True, unique = False)
-    beds = db.Column(db.String(120), index = True, unique = False)
-    baths = db.Column(db.String(50), index = False, unique = False)
-    sqft = db.Column(db.String(25), index = False, unique = False)
-    name = db.Column(db.String(120), index = True, unique = False)
-    lat = db.Column(db.Float, index = False, unique = False)
-    lon= db.Column(db.Float, index = False, unique = False)
-    connector = db.relationship('Connector', backref = 'rental', lazy = 'dynamic', cascade = 'all, delete, delete-orphan' )
+    rental_id = db.Column(db.String(50), primary_key=True)
+    address = db.Column(db.String(120), index=True, unique=False)
+    price = db.Column(db.String(50), index=False, unique=False)
+    # list_date = db.Column(db.DateTime, index=True, unique=False)  # Fixed typo in 'Columns' to 'Column'
+    beds = db.Column(db.String(120), index=True, unique=False)
+    baths = db.Column(db.String(50), index=False, unique=False)
+    sqft = db.Column(db.String(25), index=False, unique=False)
+    name = db.Column(db.String(120), index=True, unique=False)
+    lat = db.Column(db.Float, index=False, unique=False)
+    lon = db.Column(db.Float, index=False, unique=False)
+    connector = db.relationship('Connector', backref='rental', lazy='dynamic', cascade='all, delete, delete-orphan')
 
-# Restaurants within Seattle given they are walkable from the Rental Properties
 class Restaurant(db.Model):
-    restaurant_id = db.Column(db.String(50), primary_key = True)
-    address = db.Column(db.String(120), index = True, unique = False)
-    rating = db.Column(db.String(50), index = False, unique = False)
-    name = db.Column(db.String(120), index = True, unique = False)
-    lat = db.Column(db.Float, index = False, unique = False)
-    lon= db.Column(db.Float, index = False, unique = False)
-    connector = db.relationship('Connector', backref = 'restaurant', lazy = 'dynamic', cascade = 'all, delete, delete-orphan' )
-
+    restaurant_id = db.Column(db.String(50), primary_key=True)
+    address = db.Column(db.String(120), index=True, unique=False)
+    rating = db.Column(db.String(50), index=False, unique=False)
+    name = db.Column(db.String(120), index=True, unique=False)
+    lat = db.Column(db.Float, index=False, unique=False)
+    lon = db.Column(db.Float, index=False, unique=False)
+    connector = db.relationship('Connector', backref='restaurant', lazy='dynamic', cascade='all, delete, delete-orphan')
 
 class Connector(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     restaurant_id = db.Column(db.String(50), db.ForeignKey('restaurant.restaurant_id'))
     rental_id = db.Column(db.String(50), db.ForeignKey('rental.rental_id'))
