00f593ba11a311d7d72ab90265209ba823cb5b9e
Devyn Duvall <35125134+devynduvall@users.noreply.github.com>
2023-10-21 00:26:47 -0700

    updated formating

--- views.py
+++ views.py
@@ -1,27 +1,39 @@
+# Import Flask, render_template, request, and jsonify modules
 from flask import Flask, render_template, request, jsonify
 
+# Import the app, db, Rental, Restaurant, and Connector modules from the app package
 from app import app, db, Rental, Restaurant, Connector
 
+# Import the json module
 import json
 
-
-
-#a simple initial greeting
+# Define a route for the index page
 @app.route('/', methods = ["GET", "POST"])
 @app.route('/index')
 def index():
+    # Query all rentals and restaurants from the database
     rent = Rental.query.all()
-    rest = Restaurant.query.all()
+        
+    # Initialize marker_id and data variables
     marker_id = 0
     data = [] 
+    
+    # Check if the request method is POST
     if request.method == "POST":
-       marker_id = request.form["marker_id"]
-       print(marker_id)
-       data = Restaurant.query.filter_by(id = Connector.query.filter_by(rental_id = marker_id))
+        # Get the marker_id from the form data
+        marker_id = request.form["marker_id"]
+        print(marker_id)
+        
+        # Query the restaurants that match the rental_id
+        data = Restaurant.query.filter_by(id = Connector.query.filter_by(rental_id = marker_id))
+    
+    # Print the rentals to the console
     print(rent)
+    
+    # Render the index.html template with the rentals, marker_id, and data variables
     return render_template('index.html', markers = rent, marker_match = marker_id, marker_match_data = data)
 
-
+# Define a route for the marker points
 @app.route('/marker/<int:marker_id>/points')
 def get_marker_points(marker_id):
     # Use SQLAlchemy to query the database for additional points based on marker_id
@@ -31,6 +43,7 @@ def get_marker_points(marker_id):
     data = (
         db.session.query(Restaurant).filter(Restaurant.id.in_(subquery)).all()
     )
+    
     # Serialize the points into a format that can be consumed by Leaflet
     feature_collection = {
         "type": "FeatureCollection",
@@ -52,17 +65,24 @@ def get_marker_points(marker_id):
             for point in data
         ]
     }
+    
     # Return the serialized data to the client-side JavaScript code
     return jsonify(feature_collection)
 
+# Define a route for the search functionality
 @app.route('/search', methods=['POST'])
 def search():
+    # Get the search term from the form data
     search_term = request.form['search_term']
+    
+    # Query the rentals that match the search term
     data = Rental.query.filter(Rental.address.like(search_term)).all()
     print(data)
-    # Process the search term here (e.g., query a database)
+    
+    # Return the search term and data to the client-side JavaScript code
     return f"Search term: {data}"
 
+# Define a route for the about page
 @app.route('/about')
 def about():
     return render_template('about.html')
